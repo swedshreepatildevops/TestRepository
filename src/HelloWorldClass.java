debda1292dde1e4136ea0a2d26c7c8fc1f064730
chiu <swedshreepatildevops@gmail.com>
2020-12-28 21:10:40 +0530

    index.html commit

---
+++ src/HelloWorldClass.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to todllerworld.....! I am a great todller of this world.");
+
+	}
+
+}
